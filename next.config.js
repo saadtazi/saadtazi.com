f2f3e73b1b2b8c8a90739dd88bffbab851b178ea
Saad Tazi <saad.tazi@unity3d.com>
2023-04-30 18:15:29 -0400

    update some packages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['@mui/material'],
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
